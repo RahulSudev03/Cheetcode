09896c8bb04335de96ee38e1017ef89899822274
ZhangBenjamin <bluebenjamin123@gmail.com>
2024-09-04 17:34:17 -0400

    Fixed analytics

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Analytics from "@app/utils/Analytics";
+import Analytics from "./utils/Analytics";
 
 const inter = Inter({ subsets: ["latin"] });
 
