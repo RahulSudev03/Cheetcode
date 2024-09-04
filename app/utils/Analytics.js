008af96106caa1116e6a74b038ff7c9b2285ca1f
ZhangBenjamin <bluebenjamin123@gmail.com>
2024-09-04 17:47:32 -0400

    Updated google analytics again

--- app/utils/Analytics.js
+++ app/utils/Analytics.js
@@ -3,19 +3,15 @@ import Script from "next/script";
 const Analytics = () => {
     return (
         <>
-        <Script 
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-            strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-            {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
+            <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}></script>
+            <script>
+            {`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
 
-                gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
-            `}
-        </Script>
+            gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});`}
+            </script>
+
         </>
     );
 };
